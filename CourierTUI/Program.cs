e14c5e20492c5f27c89bd74aa08390e9be1b9e1b
Sean <seanshouchristensen@gmail.com>
2026-06-21 18:43:12 +1000

    Changes to UI to match draft

--- CourierTUI/Program.cs
+++ CourierTUI/Program.cs
@@ -23,24 +23,75 @@ class Program
         var tabView = new TabView()
         {
             X = 0,
+            Y = 1,
+            Width = Dim.Percent(100),
+            Height = Dim.Percent(50)
+        };
+
+        var methodComboBox = new ComboBox()
+        {
+            X = 8,
             Y = 0,
-            Width = Dim.Fill(),
-            Height = Dim.Fill()
+            Width = 15,
+            Height = 5
+        };
+
+        methodComboBox.SetSource(new string[] { "GET", "POST" });
+
+        var methodLabel = new Label()
+        {
+            X = 1,
+            Y = 0,
+            Width = 0,
+            Height = 0,
+            Text = "Method:"
+        };
+
+
+        var URLlabel = new Label()
+        {
+            X = 25,
+            Y = 0,
+            Width = 30,
+            Text = "URL: "
+        };
+
+        var URLinput = new TextField()
+        {
+            X = 29,
+            Y = 0,
+            Width = 60
+        };
+
+
+        var tabView2 = new TabView()
+        {
+            X = 0,
+            Y = 10,
+            Width = Dim.Percent(100),
+            Height = Dim.Percent(50)
         };
 
         DataHandler dataHandler = new DataHandler();
 
         ParametersTab paramsTab = new ParametersTab(dataHandler);
-        DetailsTab detailsTab = new DetailsTab(dataHandler);
         BodyTab bodyTab = new BodyTab(dataHandler);
 
-        tabView.AddTab(detailsTab.tab, true);
         tabView.AddTab(paramsTab.tab, true);
         tabView.AddTab(bodyTab.tab, true);
 
         tabView.SelectedTab = tabView.Tabs.First();
 
+        win.Add(methodComboBox);
+        win.Add(methodLabel);
+        win.Add(URLlabel);
+        win.Add(URLinput);
+
         win.Add(tabView);
+        //win.Add(tabView2);
+
+        
+
         top.Add(win);
 
         Application.Run();
